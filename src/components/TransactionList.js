da20b5e1f11711b85dca2e21a917471e749a28a2
PECHCHANAN SUWANRAK <6710110298@psu.ac.th>
2024-12-18 22:15:51 +0700

    add edit buttom

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,9 +1,23 @@
 import React from "react"
-import { Button, Table, Space, Tag, Popconfirm, Modal } from "antd"
-import { DeleteOutlined, BugOutlined } from '@ant-design/icons';
+import { Button, Table, Space, Tag, Popconfirm, Modal, Form, Input } from "antd"
+import { DeleteOutlined, BugOutlined, EditOutlined } from '@ant-design/icons';
 import dayjs from "dayjs";
 
 export default function TransactionList(props) {
+
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [editingRecord, setEditingRecord] = React.useState(null);
+
+  const handleEdit = (record) => {
+    setEditingRecord(record);
+    setIsModalOpen(true);
+  };
+
+  const handleSave = (values) => {
+    props.onRowEdited(editingRecord.id, values);
+    setIsModalOpen(false);
+    setEditingRecord(null);
+  };
 
   const columns = [
     { 
@@ -22,6 +36,11 @@ export default function TransactionList(props) {
     {
       title: "Action", key: "action", render: (_, record) => (
         <Space size="middle">
+          <Button 
+            type="primary" 
+            shape="circle" 
+            icon={<EditOutlined />} 
+            onClick={() => handleEdit(record)} />
           
           <Popconfirm
             title="Delete the transaction"
@@ -33,10 +52,11 @@ export default function TransactionList(props) {
               shape="circle" 
               icon={<DeleteOutlined />} />
           </Popconfirm>
+
           <Button 
             type="primary" 
             shape="circle" 
-            icon={<BugOutlined/>} 
+            icon={<BugOutlined />} 
             onClick={() => {
               Modal.info({
                 title: "Debug",
@@ -46,11 +66,66 @@ export default function TransactionList(props) {
         </Space>
       ), 
     },
-  ]
+  ];
 
   return (
     <>
-    <Table columns={columns} dataSource={props.data}/>
+      <Table columns={columns} dataSource={props.data} rowKey="id" />
+      
+      {/* Modal for Editing */}
+      <Modal
+        title="Edit Transaction"
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        onOk={() => {
+          const form = document.querySelector('#edit-form');
+          form.dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }));
+        }}
+      >
+        {editingRecord && (
+          <Form
+            id="edit-form"
+            initialValues={{
+              action_datetime: dayjs(editingRecord.action_datetime).format("YYYY-MM-DDTHH:mm"),
+              type: editingRecord.type,
+              amount: editingRecord.amount,
+              note: editingRecord.note,
+            }}
+            onFinish={handleSave}
+          >
+            <Form.Item
+              name="action_datetime"
+              label="Date-Time"
+              rules={[{ required: true, message: "Please input the date and time!" }]}
+            >
+              <Input type="datetime-local" />
+            </Form.Item>
+
+            <Form.Item
+              name="type"
+              label="Type"
+              rules={[{ required: true, message: "Please select a type!" }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              name="amount"
+              label="Amount"
+              rules={[{ required: true, message: "Please input the amount!" }]}
+            >
+              <Input type="number" />
+            </Form.Item>
+
+            <Form.Item
+              name="note"
+              label="Note"
+            >
+              <Input.TextArea />
+            </Form.Item>
+          </Form>
+        )}
+      </Modal>
     </>
-  )
+  );
 }
